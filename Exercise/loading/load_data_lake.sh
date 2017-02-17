22e2f998cce28125f55e45f8f73ce852d38bb308
carleeprice <carleeprice@hotmail.com>
2017-02-17 04:34:49 -0500

    Data Lake executable code

---
+++ Exercise/loading/load_data_lake.sh
@@ -0,0 +1,30 @@
+git clone https://github.com/carleeprice/W205.git
+cd W205/Exercise/loading_and_modelling
+ls
+unzip hospital.zip
+ls
+du -s TEC.csv
+
+tail -n +2 TEC.csv  > TEC_nohead.csv
+tail -n +2 TECS.csv  > TECS_nohead.csv
+tail -n +2 HGI.csv  > HGI_nohead.csv
+tail -n +2 HVBP.csv  > HVBP_nohead.csv
+tail -n +2 RAD.csv  > RAD_nohead.csv
+ls
+
+hdfs dfs -ls
+
+hdfs dfs -mkdir /user/w205/hospital_compare/TEC
+hdfs dfs -put /home/w205/W205/Exercise/loading_and_modelling/TEC_nohead.csv /user/w205/hospital_compare/TEC
+
+hdfs dfs -mkdir /user/w205/hospital_compare/TECS
+hdfs dfs -put /home/w205/W205/Exercise/loading_and_modelling/TECS_nohead.csv /user/w205/hospital_compare/TECS
+
+hdfs dfs -mkdir /user/w205/hospital_compare/HGI
+hdfs dfs -put /home/w205/W205/Exercise/loading_and_modelling/HGI_nohead.csv /user/w205/hospital_compare/HGI
+
+hdfs dfs -mkdir /user/w205/hospital_compare/HVBP
+hdfs dfs -put /home/w205/W205/Exercise/loading_and_modelling/HVBP_nohead.csv /user/w205/hospital_compare/HVBP
+
+hdfs dfs -mkdir /user/w205/hospital_compare/RAD
+hdfs dfs -put /home/w205/W205/Exercise/loading_and_modelling/RAD_nohead.csv /user/w205/hospital_compare/RAD
