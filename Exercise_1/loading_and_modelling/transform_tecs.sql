7324214444bb64adf3aee7f5f13b425b7f687de3
carleeprice <carleeprice@hotmail.com>
2017-02-23 10:15:38 -0500

    Delete transform_tecs.sql

--- Exercise_1/loading_and_modelling/transform_tecs.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE state_standards;
-
-CREATE TABLE state_standards AS
-SELECT 
-h_state as h_state, 
-condition as condition,
-measure as measure,
-CAST(score as FLOAT) as score
-FROM tecs_raw;
