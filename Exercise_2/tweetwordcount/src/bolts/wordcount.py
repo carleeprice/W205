23bc392dbc2656151070f938a6eb744fc0d7a791
carleeprice <carleeprice@hotmail.com>
2017-04-11 04:54:17 -0400

    Update wordcount.py

--- Exercise_2/tweetwordcount/src/bolts/wordcount.py
+++ Exercise_2/tweetwordcount/src/bolts/wordcount.py
@@ -9,34 +9,34 @@ class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
-		self.conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+	self.conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 
     def process(self, tup):
 
         word = tup.values[0]
 
-		self.conn = psycopg2.connect(database = "tcount", user = "postgres", password = "pass", host = "localhost", port = "5432")
-		cur = self.conn.cursor()
-		cur.execute("SELECT word FROM tweetwordcount")
+	self.conn = psycopg2.connect(database = "tcount", user = "postgres", password = "pass", host = "localhost", port = "5432")
+	cur = self.conn.cursor()
+	cur.execute("SELECT word FROM tweetwordcount")
         cur.fetchall()
 
-		if cur.rowcount == 0:
-			squery = cur.mogrify("INSERT INTO tweetwordcount (word, count) VALUES (%s, 1)", (word,))
-			cur.execute(squery)
+	if cur.rowcount == 0:
+		squery = cur.mogrify("INSERT INTO tweetwordcount (word, count) VALUES (%s, 1)", (word,))
+		cur.execute(squery)
 		
-		else:
-			squery = cur.mogrify("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word,))
-			cur.execute(squery)
+	else:
+		squery = cur.mogrify("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word,))
+		cur.execute(squery)
 			
 
-		self.conn.commit()
+	self.conn.commit()
 
-		#cur.execute("SELECT word, count from tweetwordcount")
-		#records = cur.fetchall()
-		#for rec in records:
-			#print "word =", rec[0]
-			#print "count =", rec[1], "\n"
+	#cur.execute("SELECT word, count from tweetwordcount")
+	#records = cur.fetchall()
+	#for rec in records:
+		#print "word =", rec[0]
+		#print "count =", rec[1], "\n"
 			
-		self.emit([word, self.counts[word]])
+	self.emit([word, self.counts[word]])
 		
-		self.log('%s, %d' % (word, self,counts[word])
+	self.log('%s, %d' % (word, self,counts[word])
